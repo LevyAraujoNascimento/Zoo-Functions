c9340f5bc7dbd244f89050a19c1d4729e0148a46
Levy Araujo <levy4raujo@gmail.com>
2022-11-01 13:18:36 -0300

    1º requisito concluído

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  let result = [];
+  if (ids === undefined) {
+    return [];
+  }
+  for (let index = 0; index < ids.length; index++) {
+    result.push((data.species.filter((element) => element.id === ids[index]))[0]);    
+  }
+  return result;
 }
 
 module.exports = getSpeciesByIds;
