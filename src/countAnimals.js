2da6aaa0aba87e918d8af77aa5ebbf21dc0dccd1
Levy Araujo <levy4raujo@gmail.com>
2022-11-03 16:58:46 -0300

    5º requisito concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const result = {};
+  if (animal === undefined) {
+    for (let index = 0; index < data.species.length; index += 1) {
+      const ficha = data.species[index];
+      const nome = ficha.name;
+      const quantidade = ficha.residents.length;
+      Object.defineProperty(result, nome, {
+        value: quantidade,
+        enumerable: true,
+      });
+    }
+    return result;
+  }
+  const ficha = data.species.find((element) => element.name === animal.specie);
+  if (animal.sex === undefined) {
+    return ficha.residents.length;
+  }
+  const animais = ficha.residents.filter((element) => element.sex === animal.sex);
+  return animais.length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,27 +3,22 @@ const data = require('../data/zoo_data');
 function isManager(id) {
   let gerentes = [];
   for (let index = 0; index < data.employees.length; index += 1) {
-      gerentes = gerentes.concat(data.employees[index].managers);   
+    gerentes = gerentes.concat(data.employees[index].managers);   
   }
   return gerentes.some((element) => element === id);
 }
 
 function getRelatedEmployees(managerId) {
-  try {
-      if (isManager(managerId) === false) {
-        throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  if (isManager(managerId) === true) {
+    const result = [];
+    for (let index = 0; index < data.employees.length; index += 1) {
+      const encarregados = data.employees[index].managers;
+      if (encarregados.includes(managerId) === true) {
+        result.push(`${data.employees[index].firstName} ${data.employees[index].lastName}`);
       }
-      const result = [];
-      for (let index = 0; index < data.employees.length; index += 1) {
-        const encarregados = data.employees[index].managers;
-        if (encarregados.includes(managerId) === true) {
-          result.push(`${data.employees[index].firstName} ${data.employees[index].lastName}`);
-        }
-      }
-      return result;
-  } catch (error) {
-      return error.message;
+    }
+    return result;
   }
 }
-console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c099'));
 module.exports = { isManager, getRelatedEmployees };
