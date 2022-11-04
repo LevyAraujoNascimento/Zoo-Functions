d95d1616bd2799f193af70948ef42d92f49772f9
Levy Araujo <levy4raujo@gmail.com>
2022-11-04 16:38:05 -0300

    10º requisito concluído

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { responsibleFor } = data.employees.find((element) => element.id === id);
+  const ficha = data.species.find((element) => element.id === responsibleFor[0]);
+  const oldest = ficha.residents.sort((a, b) => b.age - a.age)[0];
+  const { name, sex, age } = oldest;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
