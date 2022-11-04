ceba83734f66f21aa7efff4017377f120088fe44
Levy Araujo <levy4raujo@gmail.com>
2022-11-04 17:35:51 -0300

    6º e 7º requisitos teste Lint 1

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,31 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-    it('Teste Count', () => {
-        expect(handlerElephants('count')).toEqual(4);
-    });
+  it('Teste Count', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
 
-    it('Teste Names', () => {
-        expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
-    });
+  it('Teste Names', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
 
-    it('Teste averageAge', () => {
-        expect(handlerElephants('averageAge')).toBe(10.5);
-    });
+  it('Teste averageAge', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
 
-    it('Teste location', () => {
-        expect(handlerElephants('location')).toEqual('NW');
-    });
+  it('Teste location', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
 
-    it('Teste popularity', () => {
-        expect(handlerElephants('popularity')).toBe(5);
-    });
+  it('Teste popularity', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
 
-    it('Teste availability', () => {
-        expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
-    });
+  it('Teste availability', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
 
-    it('Teste sem parametros', () => {
-        expect(handlerElephants()).toBeUndefined();
-    });
+  it('Teste sem parametros', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
 });
