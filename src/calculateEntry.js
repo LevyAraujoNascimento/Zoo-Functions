8416ba7faa202b4921849ab62809c2133998491e
Levy Araujo <levy4raujo@gmail.com>
2022-11-04 11:45:01 -0300

    8º requisito concluído

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+  const result = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return (data.prices.adult*adult)+(data.prices.child*child)+(data.prices.senior*senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,9 +1,11 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) { 
+    //refazer
 }
 
 function getRelatedEmployees(managerId) { 
+    //refazer
 }
 
 module.exports = { isManager, getRelatedEmployees };
