fd68d1b893e317c9ccb16de266f9444c30c48af7
Levy Araujo <levy4raujo@gmail.com>
2022-11-07 14:11:02 -0300

    4º requisito corrigido Teste Lint 1

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,21 +3,21 @@ const data = require('../data/zoo_data');
 function isManager(id) {
   let gerentes = [];
   for (let index = 0; index < data.employees.length; index += 1) {
-      gerentes = gerentes.concat(data.employees[index].managers);   
+    gerentes = gerentes.concat(data.employees[index].managers);  
   }
   return gerentes.some((element) => element === id);
 }
 
-
 function getRelatedEmployees(managerId) {
   if (isManager(managerId) === false) {
-    throw  new  Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   const result = data.employees.map((element) => {
     const gerentes = element.managers;
-    if (gerentes.some((element) => element === managerId)) {
-      return element.firstName + " " + element.lastName;
+    if (gerentes.some((e) => e === managerId)) {
+      return `${element.firstName} ${element.lastName}`;
     }
+    return undefined;
   });
   return result.filter((element) => element !== undefined);
 }
