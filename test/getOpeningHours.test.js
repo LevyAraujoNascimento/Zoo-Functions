0d04155cb8c137a660c3fd359c1428cf5bc7d8c1
Levy Araujo <levy4raujo@gmail.com>
2022-11-07 12:41:21 -0300

    12º e 13º requisito Teste Lint 1

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -3,58 +3,58 @@ const getOpeningHours = require('../src/getOpeningHours');
 describe('Testes da função getOpeningHours', () => {
   it('Teste sem parametros', () => {
     const expected = {
-        Tuesday: { open: 8, close: 6 },
-        Wednesday: { open: 8, close: 6 },
-        Thursday: { open: 10, close: 8 },
-        Friday: { open: 10, close: 8 },
-        Saturday: { open: 8, close: 10 },
-        Sunday: { open: 8, close: 8 },
-        Monday: { open: 0, close: 0 }
-    };      
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
     expect(getOpeningHours()).toEqual(expected);
   });
 
   it('Teste dia e hora AM aberto', () => {
     const expected = 'The zoo is open';
-    expect(getOpeningHours('Tuesday','9:00-AM')).toEqual(expected);
+    expect(getOpeningHours('Tuesday', '9:00-AM')).toEqual(expected);
   });
-  
+
   it('Teste dia e hora PM aberto', () => {
     const expected = 'The zoo is open';
-    expect(getOpeningHours('Friday','2:00-PM')).toEqual(expected);
+    expect(getOpeningHours('Friday', '2:00-PM')).toEqual(expected);
   });
 
   it('Teste dia e hora AM fechado', () => {
     const expected = 'The zoo is closed';
-    expect(getOpeningHours('Wednesday','2:00-AM')).toEqual(expected);
+    expect(getOpeningHours('Wednesday', '2:00-AM')).toEqual(expected);
   });
 
   it('Teste dia e hora PM fechado', () => {
     const expected = 'The zoo is closed';
-    expect(getOpeningHours('Saturday','10:00-PM')).toEqual(expected);
+    expect(getOpeningHours('Saturday', '10:00-PM')).toEqual(expected);
   });
 
   it('Teste dia errado', () => {
-    expect(() => getOpeningHours('day','11:00-PM')).toThrow(/^The day must be valid. Example: Monday$/);
+    expect(() => getOpeningHours('day', '11:00-PM')).toThrow(/^The day must be valid. Example: Monday$/);
   });
-  
+
   it('Teste abreviação errada', () => {
-    expect(() => getOpeningHours('Monday','10:00-LM')).toThrow(/^The abbreviation must be \'AM\' or \'PM\'$/);
+    expect(() => getOpeningHours('Monday', '10:00-LM')).toThrow(/^The abbreviation must be \'AM\' or \'PM\'$/);
   });
 
   it('Teste hora errada', () => {
-    expect(() => getOpeningHours('Monday','Levy:00-AM')).toThrow(/^The hour should represent a number$/);
+    expect(() => getOpeningHours('Monday', 'Levy:00-AM')).toThrow(/^The hour should represent a number$/);
   });
 
   it('Teste minuto errado', () => {
-    expect(() => getOpeningHours('Monday','10:Levy-AM')).toThrow(/^The minutes should represent a number$/);
+    expect(() => getOpeningHours('Monday', '10:Levy-AM')).toThrow(/^The minutes should represent a number$/);
   });
 
   it('Teste valor da hora', () => {
-    expect(() => getOpeningHours('Monday','14:00-AM')).toThrow(/^The hour must be between 0 and 12$/);
+    expect(() => getOpeningHours('Monday', '14:00-AM')).toThrow(/^The hour must be between 0 and 12$/);
   });
 
   it('Teste valor do minuto', () => {
-    expect(() => getOpeningHours('Monday','10:80-AM')).toThrow(/^The minutes must be between 0 and 59$/);
+    expect(() => getOpeningHours('Monday', '10:80-AM')).toThrow(/^The minutes must be between 0 and 59$/);
   });
 });
