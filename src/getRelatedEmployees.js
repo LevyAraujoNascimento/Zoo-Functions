1439ca784b94798f3de1fee0bcc83b5e11048139
Levy Araujo <levy4raujo@gmail.com>
2022-11-07 14:01:54 -0300

    4º requisito corrigido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return id;
+  let gerentes = [];
+  for (let index = 0; index < data.employees.length; index += 1) {
+      gerentes = gerentes.concat(data.employees[index].managers);   
+  }
+  return gerentes.some((element) => element === id);
 }
 
+
 function getRelatedEmployees(managerId) {
-  return isManager(managerId);
+  if (isManager(managerId) === false) {
+    throw  new  Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const result = data.employees.map((element) => {
+    const gerentes = element.managers;
+    if (gerentes.some((element) => element === managerId)) {
+      return element.firstName + " " + element.lastName;
+    }
+  });
+  return result.filter((element) => element !== undefined);
 }
 
 module.exports = { isManager, getRelatedEmployees };
