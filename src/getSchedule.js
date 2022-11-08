117fa84bae359441610ad6f998b79a99f1513adb
Levy Araujo <levy4raujo@gmail.com>
2022-11-08 08:40:08 -0300

    9º requisito corrigido

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,52 @@
 const data = require('../data/zoo_data');
 
+const diasAnimal = (animal) => {
+  const ficha = data.species.find((element) => element.name === animal);
+  return ficha.availability;
+};
+
+const getAnimals = (dia) => {
+  const animais = [];
+  data.species.forEach((element) => {
+    if (element.availability.some((e) => e === dia)) {
+      animais.push(element.name);
+    }
+  });
+  return animais;
+};
+
+const horarioDia = (dia) => {
+  const { open, close } = data.hours[dia];
+  result = {};
+  let ofcH = '';
+  let exht = '';
+  if (open === 0 && close === 0) {
+    ofcH = 'CLOSED';
+    exht = 'The zoo will be closed!';
+  } else {
+    ofcH = `Open from ${open}am until ${close}pm`;
+    exht = getAnimals(dia);
+  }
+  result[dia] = {
+    officeHour: ofcH,
+    exhibition: exht,
+  };
+  return result;
+};
+
 function getSchedule(scheduleTarget) {
-  console.log(scheduleTarget);
+  const dias = Object.keys(data.hours);
+  const result = {};
+  if (data.species.some((element) => element.name === scheduleTarget)) {
+    return diasAnimal(scheduleTarget);
+  }
+  if (dias.some((element) => element === scheduleTarget)) {
+    return horarioDia(scheduleTarget);
+  }
+  dias.forEach((element) => {
+    result[element] = horarioDia(element)[element];
+  });
+  return result;
 }
 
 module.exports = getSchedule;
